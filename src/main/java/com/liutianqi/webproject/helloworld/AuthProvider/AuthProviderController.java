efe6c5c68b882ed7881a617f0f76aee0df21d452
xifenglieying <1368396070@qq.com>
2019-12-15 15:55:48 +0800

    sshkey

--- src/main/java/com/liutianqi/webproject/helloworld/AuthProvider/AuthProviderController.java
+++ src/main/java/com/liutianqi/webproject/helloworld/AuthProvider/AuthProviderController.java
@@ -1,16 +1,13 @@
 package com.liutianqi.webproject.helloworld.AuthProvider;
 
 import com.liutianqi.webproject.helloworld.dto.GithubUser;
-import com.sun.deploy.net.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.thymeleaf.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class AuthProviderController {
@@ -21,13 +18,13 @@ public class AuthProviderController {
     @GetMapping("/callback")
     public String callBack(@RequestParam("code") String code, HttpServletRequest httpServletRequest, Model model) {
         // 防止浏览器地址栏恶意输入callback?code=
-        if(httpServletRequest.getSession().getAttribute("user") != null){
+        if (httpServletRequest.getSession().getAttribute("user") != null) {
             return "redirect:";
         }
         System.out.println("code:" + code);
         String accessToken = githubProvider.getAccessToken(code);
         GithubUser githubUser = githubProvider.getUser(accessToken);
-        if(githubUser != null){
+        if (githubUser != null) {
             httpServletRequest.getSession().setAttribute("user", githubUser);
             return "redirect:";
         }
