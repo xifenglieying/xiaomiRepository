99d59c00d5f044161036b9fcfd47112b8337cec3
xifenglieying <1368396070@qq.com>
2020-01-14 17:21:08 +0800

    increase a method of saving the article

--- src/main/java/com/liutianqi/webproject/helloworld/mapper/PublishMapper.java
+++ src/main/java/com/liutianqi/webproject/helloworld/mapper/PublishMapper.java
@@ -1,5 +1,11 @@
 package com.liutianqi.webproject.helloworld.mapper;
 
 
+import com.liutianqi.webproject.helloworld.dto.Publish;
+import org.apache.ibatis.annotations.Insert;
+
 public interface PublishMapper {
+
+    @Insert("insert into Publish values (#{question}, #{tag}, #{title})")
+    void insertPublish(Publish publish);
 }
